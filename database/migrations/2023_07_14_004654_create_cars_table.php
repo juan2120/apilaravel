d72fd3efa675d0095b22bb22b53e5aaf4f992f1c
antoniov549 <2120100443@soy.utj.edu.mx>
2023-07-13 19:25:25 -0600

    proyecto con postma

---
+++ database/migrations/2023_07_14_004654_create_cars_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title',150);
+            $table->string('description',150);
+            $table->double('price');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::post('/api/registro', '\App\Http\Controllers\UserController@register');
+Route::post('/api/acceso', '\App\Http\Controllers\UserController@login');
+
+Route::resource('/api/carros', '\App\Http\Controllers\carcontroller');
